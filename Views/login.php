6c236d907172dafc4bee081ae224d16972a86421
Unknown <thomaspoint@gmail.com>
2017-11-20 16:56:22 +0100

    Push Login.css

--- Views/login.php
+++ Views/login.php
@@ -4,13 +4,18 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./Assets/CSS/login.css">
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
   <title>Login</title>
 </head>
 <body>
-  <form class="Login" action="index.html" method="post">
-    <input type="text" name="user" value="user">
-    <input type="text" name="password" value="password">
-    <input type="submit" name="Login" value="Login">
-  </form>
+    <div id="formLogin">
+      <h2>Login</h2>
+      <form class="Login" action="index.html" method="post">
+        <input id="userLogin" type="text" name="user" value="user">
+        <input id="passwordLogin" type="text" name="password" value="password">
+        <input id="buttonLogin" type="submit" name="Login" value="Login">
+      </form>
+    </div>
 </body>
 </html>
