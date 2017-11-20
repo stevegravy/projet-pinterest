2d712fa8044505d6df92bafcbd40053fce939a9e
Unknown <thomaspoint@gmail.com>
2017-11-20 15:38:00 +0100

    Login View

---
+++ Views/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Login</title>
+</head>
+<body>
+  <form class="Login" action="index.html" method="post">
+    <input type="text" name="user" value="user">
+    <input type="text" name="password" value="password">
+    <input type="submit" name="Login" value="Login">
+  </form>
+</body>
+</html>
