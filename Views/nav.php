d086d453fed47737a4af0da1bb4c80c05ee33a06
ksomao <somao@outlook.com>
2017-11-22 03:00:11 +0100

    nav done

---
+++ Views/nav.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
+    <link href="Assets/CSS/style.css" rel="stylesheet">
+    
+</head>
+<body>
+<nav id="navbar">
+    <a href=""><img id="logo" src="Assets/img/logo.svg" alt=""></a>
+    <div id="right_content">
+        <a href="" id="add"><img src="Assets/img/add.png" alt=""></a>
+        <ul id="menu">
+            <li class="link">Kevin</li>
+            <li class="link">Logout</li>
+        </ul>
+    </div>
+
+</nav>
+</body>
+</html>
