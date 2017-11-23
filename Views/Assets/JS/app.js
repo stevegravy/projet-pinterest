384463535623bfa5cded31d7cc43a40fd5eb53be
Unknown <thomaspoint@gmail.com>
2017-11-23 11:44:32 +0100

    Login responsive

--- Views/Assets/JS/app.js
+++ Views/Assets/JS/app.js
@@ -1,9 +1,10 @@
 var butLogin = document.getElementById('login');
 var Login = document.querySelector('.action');
 var acc_cont = document.getElementById('acc_cont');
+var butBurger = document.getElementById('threeLi');
+var menu = document.querySelector('#burger');
+var butLogin2 = document.getElementById('login2');
 
-console.log(butLogin);
-console.log(Login);
 
 butLogin.addEventListener("click", function(){
   Login.classList.remove("action");
@@ -15,4 +16,25 @@ acc_cont.addEventListener("click",function(event){
     Login.classList.remove("actionOnclick");
     Login.classList.add("action");
   }
+});
+
+butBurger.addEventListener("click", function(){
+  if(menu.classList.contains("visible")) {
+    menu.classList.remove("visible");
+    } else {
+    menu.classList.add("visible");
+  }
+});
+
+
+acc_cont.addEventListener("click",function(event){
+  if (!menu.contains(event.target)){
+    menu.classList.remove("visible");
+  }
+});
+
+butLogin2.addEventListener("click", function(){
+  Login.classList.remove("action");
+  Login.classList.add("actionOnclick");
+  menu.classList.remove('visible');
 });
