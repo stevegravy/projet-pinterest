9812372b9a5bd05699157896c5cb5f8cb62b4d9e
Unknown <thomaspoint@gmail.com>
2017-11-24 14:36:13 +0100

    ImageController

--- Controller/ImageController.php
+++ Controller/ImageController.php
@@ -1,34 +1,41 @@
 <?php
+require('./Model/Image.php');
 
 class ImageController
 {
+  public $ImageModel;
+
     public function __construct()
     {
+      $this->ImageModel = new Image();
     }
 
-    public function uploadImage()
+    public function uploadImage($image)
     {
-
+        $this->ImageModel->insert($image);
     }
 
     public function getImages()
     {
-
+      $this->ImageModel->all();
+      require "../Views/acceuil.php";
     }
 
-    public function getImagesFromUser()
-    {
-
-    }
+    // public function getImagesFromUser()
+    // {
+    //     $this->ImageModel->byId($image);
+    // }
 
     public function editImage()
     {
-
+        $this->ImageModel->update($image);
+        header('location:');
     }
 
     public function deleteImage()
     {
-        
+        $this->ImageModel->delete($image);
+        header('location:');
     }
 
 }
