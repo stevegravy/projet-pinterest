02b9f5e4e70c305f0b3990850984fd3f9ae69cec
ksomao <somao@outlook.com>
2017-11-27 16:37:52 +0100

    update upload methode

--- Controller/ImageController.php
+++ Controller/ImageController.php
@@ -20,13 +20,32 @@ class ImageController
         $description = $_POST['description'];
         $imagePath = "public/images/{$title}.jpg";
 
-        $img = $this->manager->make($_FILES['image']['tmp_name']);
-        $img->save($imagePath);
 
-        $image = $this->dataToObject([$title, $description, $imagePath]);
-        $this->ImageModel->insert($image);
+        if (isset($_FILES['image'])) {
+            $errors = array();
+            $file_name = $_FILES['image']['name'];
+            $file_size = $_FILES['image']['size'];
+            $file_tmp = $_FILES['image']['tmp_name'];
+            $file_type = $_FILES['image']['type'];
+            $file_ext = strtolower(end(explode('.', $_FILES['image']['name'])));
+
+            $expensions = array("jpeg", "jpg", "png");
+
+            if (in_array($file_ext, $expensions) === false) {
+                $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+            }
+            if (empty($errors) == true) {
+                move_uploaded_file($file_tmp, "public/images/" . $title . "." . $file_ext);
+                $image = $this->dataToObject([$title, $description, $imagePath]);
+                $this->ImageModel->insert($image);
+                echo "Success";
+            } else {
+                print_r($errors);
+            }
+        }
 
         header("location:index.php?action=accueil");
+
     }
 
     public function getImages()
