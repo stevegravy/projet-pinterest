bc663a60169ed1d3b66d794d8b6752266cebe9cf
ksomao <somao@outlook.com>
2017-11-27 15:06:00 +0100

    update

--- Views/accueil.php
+++ Views/accueil.php
@@ -1,18 +1,16 @@
 <div id="acc_cont">
-    <div class="overlay">
-        <form id="searchForm" action="index?action=searchImage">
-            <input id="searchBar" type="text" placeholder="search">
-        </form>
-        <div class='grid'>
-            <?php foreach ($images as $image)
-                echo "    
+    <form id="searchForm" action="index?action=searchImage">
+        <input id="searchBar" type="text" placeholder="search">
+    </form>
+    <div class='grid'>
+        <?php foreach ($images as $image)
+            echo "    
         <div class='grid-item'>
             <img src='{$image->chemin}'>
             <div class='desc'>
                 <h4>{$image->titre}</h4>
             </div>";
-            ?>
-        </div>
+        ?>
     </div>
 </div>
 </div>
