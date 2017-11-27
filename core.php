ade239d4df1238d981650876a966f6ac7498f2d0
Unknown <thomaspoint@gmail.com>
2017-11-27 09:47:41 +0100

    da

--- core.php
+++ core.php
@@ -1,8 +1,8 @@
 <?php
 
 $config = (object) [
-    'name' => 'pinterest',
+    'name' => 'test',
     'username' => 'root',
-    'password' => '',
+    'password' => 'user',
     'connection' => 'mysql:host=localhost'
 ];
